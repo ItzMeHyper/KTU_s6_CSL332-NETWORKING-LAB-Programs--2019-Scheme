9efd27e85a126e7239efa950d4c26765ede14457
ItzMeHyper <vaisakhmanu0005@gmail.com>
2026-02-22 11:58:05 +0530

    DVR.c

--- DVR.c
+++ DVR.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
 
-struct node{
+struct node {
     unsigned dist[20];
     unsigned from[20];
 } rt[10];
 
-int main(){
+int main() {
     int costmat[20][20];
     int nodes, i, j, k, count = 0;
+
     printf("\nEnter the number of nodes : ");
     scanf("%d", &nodes);
+
     printf("\nEnter the cost matrix :\n");
-    for (i = 0; i < nodes; i++){
-        for (j = 0; j < nodes; j++){
+    for (i = 0; i < nodes; i++) {
+        for (j = 0; j < nodes; j++) {
             scanf("%d", &costmat[i][j]);
+
             costmat[i][i] = 0;
-            rt[i].dist[j] = costmat[i][j]; // initialize the distance equal to cost matrix
-            rt[i].from[j] = j;             // initialize the source node
+            rt[i].dist[j] = costmat[i][j];   // Initialize distance equal to cost matrix
+            rt[i].from[j] = j;               // Initialize source node
         }
     }
-    do{
+
+    do {
         count = 0;
-        for (i = 0; i < nodes; i++) // We choose arbitrary vertex k and we calculate the direct distance from node i to k using cost matrix.
-                // and add the distance from k to node j
-                for (j = 0; j < nodes; j++) for (k = 0; k < nodes; k++) if (rt[i].dist[j] > costmat[i][k] + rt[k].dist[j]) { // We calculate the minimum distance
-                rt[i].dist[j] = rt[i].dist[k] + rt[k].dist[j];
-                rt[i].from[j] = k;
-                count++;
+
+        for (i = 0; i < nodes; i++) {
+            // Choose arbitrary vertex k
+            for (j = 0; j < nodes; j++) {
+                for (k = 0; k < nodes; k++) {
+                    // Calculate minimum distance
+                    if (rt[i].dist[j] > costmat[i][k] + rt[k].dist[j]) {
+                        rt[i].dist[j] = rt[i].dist[k] + rt[k].dist[j];
+                        rt[i].from[j] = k;
+                        count++;
+                    }
+
+                }
             }
+        }
+
     } while (count != 0);
-    
+
     for (i = 0; i < nodes; i++){
-        printf("\n\n For Router %d\n", i + 1);
+        printf("\nFor Router %d\n", i + 1);
         for (j = 0; j < nodes; j++){
-            printf("\t\nNode %d via %d Distance %d ", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
+            printf("\tNode %d via %d Distance %d \n", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
         }
     }
-    printf("\n\n");
+    printf("\n");
 }
